8b296eb9c46b6770901faae26709219d83fc3244
Evan J. Kang <95evankang@gmail.com>
2022-03-18 01:42:14 +0000

    ha

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 var Calculator = require("./Calculator.js");//.js is optional
 var calc = new Calculator();
-let result = calc.add(2,1);
-console.log(result);
+let addResult = calc.add(2,1);
+letdivideResult = calc.divide(8/4);
+let output = `1+2=${addResult}, and 8/4=${divideResult}`;
+console.log(output);
 
